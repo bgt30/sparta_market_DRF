2abd1306c2684ddff2f4d1f02359ca794db28729
bgt30 <rhkdfla910676@gmail.com>
2024-12-30 15:39:45 +0900

    tag update

--- products/models.py
+++ products/models.py
@@ -37,3 +37,7 @@ class Tag(models.Model):
     
     def __str__(self):
         return self.name 
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super().save(*args, **kwargs) 
